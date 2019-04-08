ea570390683a0887317fff9dd1d3b2d5df5da3cd
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-04-08 16:20:29 +0300

    lesson 13 - done
    About different between component and functional element.
    
    Tests..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+//import PropTypes from 'prop-types';
 import TodoItem from './components/TodoItem';
 import todosData from './todosData';
-const App =()=> {
-    console.log(todosData);
-    const todoItems = todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>)
-    return <div className='todo-list'>
-        {todoItems}
-    </div>;
+class App extends Component {
+    sayHi = () =>{
+        console.log('HI');
+    };
+    render() {
+        this.sayHi();
+    const todoItems = todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
+        return (
+            <div className='todo-list'>
+                {todoItems}
+            </div>
+        );
+    }
 }
+
+App.propTypes = {};
 
 export default App;
