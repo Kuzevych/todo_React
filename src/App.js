9b311713984ac561966cb57ba4f5301a5766765a
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-04-08 21:57:31 +0300

    lesson 16. delete todosData and add info from todosData to state (create state before).

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, {Component} from 'react';
 //import PropTypes from 'prop-types';
 import TodoItem from './components/TodoItem';
-import todosData from './todosData';
+//import todosData from './todosData';
+
+
 class App extends Component {
-    sayHi = () =>{
-        console.log('HI');
-    };
+    constructor(){
+        super()
+        this.state = {
+            todosData: [
+                {
+                    id: 1,
+                    text: "Take out the trash",
+                    completed: true
+                },
+                {
+                    id: 2,
+                    text: "Grocery shopping",
+                    completed: true
+                },
+                {
+                    id: 3,
+                    text: "Clean gecko tank",
+                    completed: false
+                },
+                {
+                    id: 4,
+                    text: "Mow lawn",
+                    completed: true
+                },
+                {
+                    id: 5,
+                    text: "Catch up on Arrested Development",
+                    completed: false
+                },
+                {
+                    id: 6,
+                    text: "Create todo list",
+                    completed: true
+                }
+            ]
+        }
+    }
+
     render() {
-        this.sayHi();
-    const todoItems = todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
+        //console.log(this.state.todosData);
+    //const todoItems = todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
+    const todoItems = this.state.todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
         return (
             <div className='todo-list'>
                 {todoItems}
