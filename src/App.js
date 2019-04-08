fbc3909d886331f51d9a60ef7f13a9a349ece20a
Mykola Kuzevych <m.kuzevych@gmail.com>
2019-04-08 21:58:49 +0300

    mmini refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 //import PropTypes from 'prop-types';
 import TodoItem from './components/TodoItem';
+import todosData from "./todosData";
 //import todosData from './todosData';
 
 
@@ -8,45 +9,14 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            todosData: [
-                {
-                    id: 1,
-                    text: "Take out the trash",
-                    completed: true
-                },
-                {
-                    id: 2,
-                    text: "Grocery shopping",
-                    completed: true
-                },
-                {
-                    id: 3,
-                    text: "Clean gecko tank",
-                    completed: false
-                },
-                {
-                    id: 4,
-                    text: "Mow lawn",
-                    completed: true
-                },
-                {
-                    id: 5,
-                    text: "Catch up on Arrested Development",
-                    completed: false
-                },
-                {
-                    id: 6,
-                    text: "Create todo list",
-                    completed: true
-                }
-            ]
+            todos: todosData
         }
     }
 
     render() {
         //console.log(this.state.todosData);
     //const todoItems = todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
-    const todoItems = this.state.todosData.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
+    const todoItems = this.state.todos.map(item=><TodoItem key={item.id} completed={item.completed} text={item.text}/>);
         return (
             <div className='todo-list'>
                 {todoItems}
